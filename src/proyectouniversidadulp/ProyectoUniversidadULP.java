d92898a1392ef19af2a82e64e414446d46df4a93
Lucio Di Sanzo <90800611+luciomds@users.noreply.github.com>
2021-10-08 18:03:34 -0300

    Update ProyectoUniversidadULP.java

--- src/proyectouniversidadulp/ProyectoUniversidadULP.java
+++ src/proyectouniversidadulp/ProyectoUniversidadULP.java
@@ -14,17 +14,17 @@ public class ProyectoUniversidadULP {
 
     public static void main(String[] args) throws ClassNotFoundException {
         //Conexion conexion = new Conexion("jdbc:mysql://localhost/universidad","root","");
-        Conexion conexion = new Conexion();
+        Conexion conexion = new Conexion(); //CREANDO LA CONEXION   
         AlumnoData ad = new AlumnoData(conexion);
         
         List<Alumno> alumnos = new ArrayList<>();
         
-        Alumno a = new Alumno(2225, "Marcos Suarez", LocalDate.of(2000, Month.NOVEMBER, 28));
+        Alumno a = new Alumno(2225, "Marcos Suarez", LocalDate.of(2000, Month.NOVEMBER, 28)); //CREANDO ALUMNOS
         Alumno b = new Alumno(9999, "Jose Sosa", LocalDate.of(1995, Month.MARCH, 12));
-        ad.guardarAlumno(a);
+        ad.guardarAlumno(a); //AGREGANDO ALUMNOS
         ad.guardarAlumno(b);
         //ad.actualizarAlumno(a);
-        System.out.println("buscar alumno:"+ad.buscarAlumno(11));
+        System.out.println("buscar alumno:"+ad.buscarAlumno(11)); //BUSCAR ALUMNOS
         alumnos=ad.obtenerAlumnos();
         System.out.println("lista:");
         for(Alumno al : alumnos){
