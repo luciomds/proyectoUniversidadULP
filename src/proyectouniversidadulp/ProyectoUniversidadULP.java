f01b8a17f3ebfb0ba4d36216ba1021cf16685aed
Lucio Di Sanzo <luciodisanzo@gmail.com>
2021-10-12 09:25:35 -0300

    Agregue el metodo agregarMateria()
    agregue el metodo agregarMateria(). Lo probe en el main y me estuvo funcionando

--- src/proyectouniversidadulp/ProyectoUniversidadULP.java
+++ src/proyectouniversidadulp/ProyectoUniversidadULP.java
@@ -6,8 +6,10 @@ import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 import proyectouniversidadulp.control.AlumnoData;
+import proyectouniversidadulp.control.MateriaData;
 import proyectouniversidadulp.modelo.Alumno;
 import proyectouniversidadulp.modelo.Conexion;
+import proyectouniversidadulp.modelo.Materia;
 
 
 public class ProyectoUniversidadULP {
@@ -16,6 +18,7 @@ public class ProyectoUniversidadULP {
         //Conexion conexion = new Conexion("jdbc:mysql://localhost/universidad","root","");
         Conexion conexion = new Conexion(); //CREANDO LA CONEXION   
         AlumnoData ad = new AlumnoData(conexion);
+        MateriaData md = new MateriaData  (conexion);
         
         List<Alumno> alumnos = new ArrayList<>();
         
@@ -23,13 +26,18 @@ public class ProyectoUniversidadULP {
         Alumno b = new Alumno(9999, "Jose Sosa", LocalDate.of(1995, Month.MARCH, 12));
         ad.guardarAlumno(a); //AGREGANDO ALUMNOS
         ad.guardarAlumno(b);
-        //ad.actualizarAlumno(a);
+        ad.actualizarAlumno(a);
         System.out.println("buscar alumno:"+ad.buscarAlumno(11)); //BUSCAR ALUMNOS
         alumnos=ad.obtenerAlumnos();
         System.out.println("lista:");
         for(Alumno al : alumnos){
             System.out.println(al);
         } 
+        
+        
+        Materia m1 = new Materia(0123, "Laboratorio I", 2);
+        
+        md.guardarMateria(m1);
         
     }
     
--- src/proyectouniversidadulp/control/MateriaData.java
+++ src/proyectouniversidadulp/control/MateriaData.java
@@ -5,10 +5,52 @@
  */
 package proyectouniversidadulp.control;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import proyectouniversidadulp.modelo.Conexion;
+import proyectouniversidadulp.modelo.Materia;
+
 /**
  *
  * @author daniel
  */
 public class MateriaData {
-    
+    private Connection con;
+
+    public MateriaData(Conexion conexion) {
+        try {
+            this.con = conexion.getConexion();
+            System.out.println("Conectado");
+        } catch (SQLException ex) {
+            System.out.println("Error en la conexión");
+        }
+    }
+
+    public void guardarMateria(Materia materia) {
+        String sql = "INSERT INTO materia(nombre, cuatrimestre, activo) VALUES (?,?,?)";
+
+        try {
+            try (PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {  //Prepara la sentencia para SQL
+                ps.setString(1, materia.getNombre());
+                ps.setInt(2, materia.getCuatrimestre());                                
+                ps.setBoolean(3, materia.isActivo());
+
+                ps.executeUpdate(); //NO PONER PARAMETROS
+                ResultSet rs = ps.getGeneratedKeys(); //Recupero el ID (id_alumno)
+                if (rs.next()) {
+                    System.out.println("Materia " + materia.getNombre() + ", cargada correctamente.");
+                    materia.setId_materia(rs.getInt(1));
+                    //alumno.setId_alumno(rs.getInt("id_alumno"));
+                    System.out.println("id de la materia: " + materia.getId_materia());
+                }
+                ps.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error al insertar \n" + ex);
+        }
+    }
 }
